03e28de1be3a46ffd505699bcc56f1fe2d8ef69f
Niraj Kumar <npu4706@gmail.com>
2024-12-28 14:08:04 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_Chatbot',
+    version= '0.0.0',
+    author= 'Niraj Kumar',
+    author_email= 'npu4706@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
